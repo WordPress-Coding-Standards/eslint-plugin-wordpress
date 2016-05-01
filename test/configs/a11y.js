107c63ca48cae5d2f8d8daf928f1ef6012e068dc
Stephen Edgar <stephen@netweb.com.au>
2016-05-01 22:05:38 +1000

    Remove jsx-a11y

--- test/configs/a11y.js
+++
@@ -1,9 +0,0 @@
-import test from "ava";
-import isPlainObj from 'is-plain-obj';
-
-test('a11y', t => {
-	const conf = require('../../lib/configs/a11y');
-	t.true(isPlainObj(conf));
-	t.true(isPlainObj(conf.env));
-	t.true(isPlainObj(conf.rules));
-});
