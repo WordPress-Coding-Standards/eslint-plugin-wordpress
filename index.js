66d0aa346c073e5b48c952fa744fab600f665f9a
Stephen Edgar <stephen@netweb.com.au>
2016-05-03 16:21:38 +1000

    feat: Add eslint-plugin-backbone (#13)

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 module.exports = {
 	configs: {
 		ava: require('./lib/configs/ava'),
+		backbone: require('./lib/configs/backbone'),
 		i18n: require('./lib/configs/i18n'),
 		jscs: require('./lib/configs/jscs'),
 		jsdoc: require('./lib/configs/jsdoc'),
