3159f0a5580c15fbc3320311f1ae55c3f6cbcd72
Stephen Edgar <stephen@netweb.com.au>
2016-06-16 19:02:28 +1000

    style: Expected empty line after require statement not followed by another require. `import/newline-after-import`

--- test/configs/qunit.js
+++ test/configs/qunit.js
@@ -3,6 +3,7 @@ import isPlainObj from 'is-plain-obj';
 
 test('qunit', t => {
 	const conf = require('../../lib/configs/qunit');
+
 	t.true(isPlainObj(conf));
 	t.true(isPlainObj(conf.env));
 	t.true(isPlainObj(conf.rules));
--- test/configs/recommended.js
+++ test/configs/recommended.js
@@ -3,6 +3,7 @@ import isPlainObj from 'is-plain-obj';
 
 test('recommended', t => {
 	const conf = require('../../lib/configs/recommended');
+
 	t.true(isPlainObj(conf));
 	t.true(isPlainObj(conf.env));
 	t.true(isPlainObj(conf.rules));
