060c3d48441cf977ec5138e93be576fbdcd54eef
Stephen Edgar <stephen@netweb.com.au>
2017-08-01 21:18:47 +1000

    chore: Update `__tests__/rules/array-function-spacing.js` per latest ESLint config

--- __tests__/rules/array-function-spacing.js
+++ __tests__/rules/array-function-spacing.js
@@ -6,73 +6,73 @@
  */
 'use strict';
 
-//------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 // Requirements
-//------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 const rule = require( '../../lib/rules/array-function-spacing' ),
 	RuleTester = require( 'eslint' ).RuleTester;
 
-//------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 // Tests
-//------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
-( new RuleTester( { env: { es6: true } } ) ).run( 'array-function-spacing', rule, {
+( new RuleTester({ env: { es6: true } }) ).run( 'array-function-spacing', rule, {
 	valid: [
 		{
-			code: '( function( wp ) {\n	wp.foo = foo( \'bar\', {});\n	wp.baz(function() {\n		console.log( \'Done\' );\n	});\n} )( window.wp = window.wp || {} );'
+			code: '( function( wp ) {\n	wp.foo = foo( \'bar\', {});\n	wp.baz(function() {\n		console.log( \'Done\' );\n	});\n} )( window.wp = window.wp || {} );',
 		},
 		{
-			code: 'foo(function() {});'
+			code: 'foo(function() {});',
 		},
 		{
-			code: 'foo(function() {}, 1 );'
+			code: 'foo(function() {}, 1 );',
 		},
 		{
-			code: 'foo( 1, function() {});'
+			code: 'foo( 1, function() {});',
 		},
 		{
-			code: 'foo(() => {});'
+			code: 'foo(() => {});',
 		},
 		{
-			code: 'foo(() => {}, 1 );'
+			code: 'foo(() => {}, 1 );',
 		},
 		{
-			code: 'foo( 1, () => {});'
+			code: 'foo( 1, () => {});',
 		},
 		{
-			code: 'foo({});'
+			code: 'foo({});',
 		},
 		{
-			code: 'foo({}, 1 );'
+			code: 'foo({}, 1 );',
 		},
 		{
-			code: 'foo( 1, {});'
+			code: 'foo( 1, {});',
 		},
 		{
-			code: 'foo([]);'
+			code: 'foo([]);',
 		},
 		{
-			code: 'foo([], 1 );'
+			code: 'foo([], 1 );',
 		},
 		{
-			code: 'foo( 1, []);'
+			code: 'foo( 1, []);',
 		},
 		{
-			code: 'foo( 1 );'
+			code: 'foo( 1 );',
 		},
 		{
-			code: 'foo[1];'
+			code: 'foo[1];',
 		},
 		{
-			code: 'foo[\'bar\'];'
+			code: 'foo[\'bar\'];',
 		},
 		{
-			code: 'foo[`bar`];'
+			code: 'foo[`bar`];',
 		},
 		{
-			code: 'foo[ key ];'
-		}
+			code: 'foo[ key ];',
+		},
 	],
 
 	invalid: [
@@ -80,172 +80,172 @@ const rule = require( '../../lib/rules/array-function-spacing' ),
 			code: 'foo( function() {} );',
 			errors: [
 				{ message: 'There should be no whitespace before first function argument of type function' },
-				{ message: 'There should be no whitespace after last function argument of type function' }
-			]
+				{ message: 'There should be no whitespace after last function argument of type function' },
+			],
 		},
 		{
 			code: 'foo( function() {});',
 			errors: [
-				{ message: 'There should be no whitespace before first function argument of type function' }
-			]
+				{ message: 'There should be no whitespace before first function argument of type function' },
+			],
 		},
 		{
 			code: 'foo(function() {} );',
 			errors: [
-				{ message: 'There should be no whitespace after last function argument of type function' }
-			]
+				{ message: 'There should be no whitespace after last function argument of type function' },
+			],
 		},
 		{
 			code: 'foo( () => {} );',
 			errors: [
 				{ message: 'There should be no whitespace before first function argument of type function' },
-				{ message: 'There should be no whitespace after last function argument of type function' }
-			]
+				{ message: 'There should be no whitespace after last function argument of type function' },
+			],
 		},
 		{
 			code: 'foo( () => {});',
 			errors: [
-				{ message: 'There should be no whitespace before first function argument of type function' }
-			]
+				{ message: 'There should be no whitespace before first function argument of type function' },
+			],
 		},
 		{
 			code: 'foo(() => {} );',
 			errors: [
-				{ message: 'There should be no whitespace after last function argument of type function' }
-			]
+				{ message: 'There should be no whitespace after last function argument of type function' },
+			],
 		},
 		{
 			code: 'foo( {} );',
 			errors: [
 				{ message: 'There should be no whitespace before first function argument of type object' },
-				{ message: 'There should be no whitespace after last function argument of type object' }
-			]
+				{ message: 'There should be no whitespace after last function argument of type object' },
+			],
 		},
 		{
 			code: 'foo( {});',
 			errors: [
-				{ message: 'There should be no whitespace before first function argument of type object' }
-			]
+				{ message: 'There should be no whitespace before first function argument of type object' },
+			],
 		},
 		{
 			code: 'foo({} );',
 			errors: [
-				{ message: 'There should be no whitespace after last function argument of type object' }
-			]
+				{ message: 'There should be no whitespace after last function argument of type object' },
+			],
 		},
 		{
 			code: 'foo( [] );',
 			errors: [
 				{ message: 'There should be no whitespace before first function argument of type array' },
-				{ message: 'There should be no whitespace after last function argument of type array' }
-			]
+				{ message: 'There should be no whitespace after last function argument of type array' },
+			],
 		},
 		{
 			code: 'foo( []);',
 			errors: [
-				{ message: 'There should be no whitespace before first function argument of type array' }
-			]
+				{ message: 'There should be no whitespace before first function argument of type array' },
+			],
 		},
 		{
 			code: 'foo([] );',
 			errors: [
-				{ message: 'There should be no whitespace after last function argument of type array' }
-			]
+				{ message: 'There should be no whitespace after last function argument of type array' },
+			],
 		},
 		{
 			code: 'foo(1);',
 			errors: [
 				{ message: 'There should be whitespace before first function argument' },
-				{ message: 'There should be whitespace after last function argument' }
-			]
+				{ message: 'There should be whitespace after last function argument' },
+			],
 		},
 		{
 			code: 'foo( 1);',
 			errors: [
-				{ message: 'There should be whitespace after last function argument' }
-			]
+				{ message: 'There should be whitespace after last function argument' },
+			],
 		},
 		{
 			code: 'foo(1 );',
 			errors: [
-				{ message: 'There should be whitespace before first function argument' }
-			]
+				{ message: 'There should be whitespace before first function argument' },
+			],
 		},
 		{
 			code: 'foo[ 1 ];',
 			errors: [
 				{ message: 'There should be no whitespace before array number property' },
-				{ message: 'There should be no whitespace after array number property' }
-			]
+				{ message: 'There should be no whitespace after array number property' },
+			],
 		},
 		{
 			code: 'foo[ 1];',
 			errors: [
-				{ message: 'There should be no whitespace before array number property' }
-			]
+				{ message: 'There should be no whitespace before array number property' },
+			],
 		},
 		{
 			code: 'foo[1 ];',
 			errors: [
-				{ message: 'There should be no whitespace after array number property' }
-			]
+				{ message: 'There should be no whitespace after array number property' },
+			],
 		},
 		{
 			code: 'foo[ \'bar\' ];',
 			errors: [
 				{ message: 'There should be no whitespace before array string property' },
-				{ message: 'There should be no whitespace after array string property' }
-			]
+				{ message: 'There should be no whitespace after array string property' },
+			],
 		},
 		{
 			code: 'foo[ \'bar\'];',
 			errors: [
-				{ message: 'There should be no whitespace before array string property' }
-			]
+				{ message: 'There should be no whitespace before array string property' },
+			],
 		},
 		{
 			code: 'foo[\'bar\' ];',
 			errors: [
-				{ message: 'There should be no whitespace after array string property' }
-			]
+				{ message: 'There should be no whitespace after array string property' },
+			],
 		},
 		{
 			code: 'foo[ `bar` ];',
 			errors: [
 				{ message: 'There should be no whitespace before array string property' },
-				{ message: 'There should be no whitespace after array string property' }
-			]
+				{ message: 'There should be no whitespace after array string property' },
+			],
 		},
 		{
 			code: 'foo[ `bar`];',
 			errors: [
-				{ message: 'There should be no whitespace before array string property' }
-			]
+				{ message: 'There should be no whitespace before array string property' },
+			],
 		},
 		{
 			code: 'foo[`bar` ];',
 			errors: [
-				{ message: 'There should be no whitespace after array string property' }
-			]
+				{ message: 'There should be no whitespace after array string property' },
+			],
 		},
 		{
 			code: 'foo[key];',
 			errors: [
 				{ message: 'There should be whitespace before array property' },
-				{ message: 'There should be whitespace after array property' }
-			]
+				{ message: 'There should be whitespace after array property' },
+			],
 		},
 		{
 			code: 'foo[ key];',
 			errors: [
-				{ message: 'There should be whitespace after array property' }
-			]
+				{ message: 'There should be whitespace after array property' },
+			],
 		},
 		{
 			code: 'foo[key ];',
 			errors: [
-				{ message: 'There should be whitespace before array property' }
-			]
-		}
-	]
-} );
+				{ message: 'There should be whitespace before array property' },
+			],
+		},
+	],
+});
