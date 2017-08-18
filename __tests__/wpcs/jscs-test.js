a02aa25378aa328f8c2e4bc4b8aa0c136852aa45
Stephen Edgar <stephen@netweb.com.au>
2017-08-18 20:19:17 +1000

    tests: Add JSCS fixtures tests.

---
+++ __tests__/wpcs/jscs-test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const eslint = require( 'eslint' );
+const path = require( 'path' );
+const stripAnsi = require( 'strip-ansi' );
+
+const configFile = path.resolve( __dirname, '../../lib/configs/jscs.js' );
+const linter = new eslint.CLIEngine({
+	configFile,
+	ignorePath: './.gitignore',
+	useEslintrc: false,
+});
+
+describe( 'Recommended config tests', () => {
+	let formatter;
+	let report;
+
+	beforeEach(() => {
+		report = linter.executeOnFiles(['__tests__/wpcs/fixtures/*-fixture.js']);
+		formatter = stripAnsi( linter.getFormatter( 'codeframe' ) );
+	});
+
+	test( 'should have correct error and warning counts', () => {
+		expect( report.results.length ).toBe( 2 );
+		expect( report.errored ).toBeFalsy();
+		expect( report.errorCount ).toBe( 3 );
+		expect( report.warningCount ).toBe( 0 );
+		expect( report.fixableErrorCount ).toBe( 2 );
+		expect( report.fixableWarningCount ).toBe( 0 );
+	});
+
+	test( 'lint results should match snapshot', () => {
+		let output = formatter( report.results );
+		let { results } = report;
+		for (let result of results) {
+			let relativeFilePath = path.relative( __dirname, result.filePath );
+			expect( stripAnsi( output ) ).toMatchSnapshot( relativeFilePath );
+		}
+	});
+});
