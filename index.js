9f2dbffc2906f94e05037794b542faea112848f2
Stephen Edgar <stephen@netweb.com.au>
2017-08-18 15:08:44 +1000

    fix: Remove the `ava`, `backbone`, and `qunit` ESLint plugins and configs

--- index.js
+++ index.js
@@ -5,14 +5,11 @@ module.exports = {
 		'array-function-spacing': require( './lib/rules/array-function-spacing' ),
 	},
 	configs: {
-		ava: require( './lib/configs/ava' ),
-		backbone: require( './lib/configs/backbone' ),
 		i18n: require( './lib/configs/i18n' ),
 		jscs: require( './lib/configs/jscs' ),
 		jsdoc: require( './lib/configs/jsdoc' ),
 		jshint: require( './lib/configs/jshint' ),
 		node: require( './lib/configs/node' ),
-		qunit: require( './lib/configs/qunit' ),
 		recommended: require( './lib/configs/recommended' ),
 	},
 };
