899c0afd69b95005d8528501101e8d07ed394990
Stephen Edgar <stephen@netweb.com.au>
2018-03-14 14:03:45 +1100

    tests: use `strict` for the test description of strict-mode config tests

--- __tests__/configs/strict-mode.js
+++ __tests__/configs/strict-mode.js
@@ -2,7 +2,7 @@
 
 const isPlainObj = require( 'is-plain-obj' );
 
-test( 'node', () => {
+test( 'strict', () => {
 	const conf = require( '../../lib/configs/strict-mode' );
 
 	expect( isPlainObj( conf ) ).toBe( true );
